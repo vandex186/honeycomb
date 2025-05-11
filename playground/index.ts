bb2966342502e267fd3a5bd535f64f2a5bac2bbb
vandex186 <van.dex186@gmail.com>
2025-05-11 14:49:02 +0700

    Apply 3D transform to SVG

--- playground/index.ts
+++ playground/index.ts
@@ -12,40 +12,6 @@ const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
 svg.setAttribute('viewBox', '0 0 300 300')
 container?.appendChild(svg)
 
-function applyMatrix3DTransform(x: number, y: number): { x: number; y: number } {
-  // Matrix3D values from the example: matrix3d(1, 0, 0, 0, 0, 1, 0, -0.002, 0, 0, 1, 0, 0, 0, 0, 1)
-  const matrix = [
-    1, 0, 0, 0,
-    0, 1, 0, -0.002,
-    0, 0, 1, 0,
-    0, 0, 0, 1
-  ]
-  
-  // Apply transformation
-  const transformedX = x * matrix[0] + y * matrix[4] + matrix[12]
-  const transformedY = x * matrix[1] + y * matrix[5] + matrix[13]
-  
-  return { x: transformedX, y: transformedY }
-}
-
-function updateHexCoordinates() {
-  for (const hex of grid) {
-    // Store original center coordinates
-    const originalX = hex.x
-    const originalY = hex.y
-    
-    // Apply 3D transform and store result
-    hex.transformedCoordinates = applyMatrix3DTransform(originalX, originalY)
-    
-    // Update visual position
-    const group = svg.children[grid.toArray().indexOf(hex)]
-    if (group) {
-      const transform = `translate(${hex.transformedCoordinates.x - originalX} ${hex.transformedCoordinates.y - originalY})`
-      group.setAttribute('transform', transform)
-    }
-  }
-}
-
 // Initial render
 for (const hex of grid) {
   const group = document.createElementNS('http://www.w3.org/2000/svg', 'g')
@@ -65,9 +31,6 @@ for (const hex of grid) {
   group.appendChild(text)
   svg.appendChild(group)
 }
-
-// Apply initial 3D transform
-updateHexCoordinates()
 
 // Prevent default touch behaviors
 document.addEventListener('touchstart', (e) => e.preventDefault(), { passive: false })
